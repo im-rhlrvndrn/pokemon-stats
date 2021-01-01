41810a66e57fd9129d23affe40fc3915c8916ce4
Rahul Ravindran <rahulr1116@gmail.com>
2021-01-01 12:53:02 +0530

    fix: Fixed CSS bugs in the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDataLayerValue } from '../../DataLayer';
+import useWindowSize from '../../utils/useWindowSize';
+import pokemonInstance from '../../utils/pokemon';
 
 // images
 import PokemonLogo from '../../images/pokemon-logo.png';
-import pokemonInstance from '../../utils/pokemon';
+import SearchIcon from '../../images/SearchIcon';
 
 // styles
 import './Header.scss';
 
 const Header = () => {
+    const _windowSize = useWindowSize();
     const history = useHistory();
     const [{ pokemon }, dispatch] = useDataLayerValue();
     const [searchInput, setSearchInput] = useState('');
 
     const fetchPokemon = async () => {
         try {
+            if (searchInput === '') return alert('Please enter a Pokemon name');
             let response = await pokemonInstance.getPokemonByName(searchInput.toLowerCase());
-            console.log(response);
             dispatch({ type: 'SET_POKEMON', pokemon: response });
             history.push(`/pokemon/${searchInput.toLowerCase()}`);
         } catch (error) {
@@ -46,7 +49,9 @@ const Header = () => {
                         value={searchInput}
                         onChange={(event) => setSearchInput(event.target.value)}
                     />
-                    <button type='submit'>Search</button>
+                    <button type='submit'>
+                        {_windowSize?.width <= 500 ? <SearchIcon color='white' /> : 'Search'}
+                    </button>
                 </form>
             </div>
         </div>
